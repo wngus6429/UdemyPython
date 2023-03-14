e957e396989b744840a86dd8a8efe8ed87f58378
ParkJuhyun <wngus6429@gmail.com>
2023-03-14 19:29:44 +0900

    tmp

---
+++ 사전.py
@@ -0,0 +1,10 @@
+datadict = { "juhyun": "good", "whatscore":100}
+
+print(datadict.items())
+print(datadict.keys())
+print(datadict.values())
+print(datadict)
+print("juhyun" in datadict)
+
+tu = (1, 2, 3, 4, 5)
+print(tu[1:])
