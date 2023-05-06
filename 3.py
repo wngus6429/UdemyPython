68cfc3dde46b83b747cdcf341d8204c59a884993
ParkJuhyun <wngus6429@gmail.com>
2023-05-06 16:34:01 +0900

    update temp

--- 3.py
+++ 3.py
@@ -1,10 +1,8 @@
 import math
 # 수학함수 라이브러리 부르기
-result = math.sqrt(49)  # 제곱근
-print(result) #결과5
-print(help(math)) #수학에 관한 정보들이 나옴
+result = math.sqrt(49) # 제곱근
+print(result) # 결과5
+print(help(math)) # 수학에 관한 정보들이 나옴
 
 # y = math.log2(10)
 # print(y)
-
-
