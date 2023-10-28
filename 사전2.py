5ec6ed71f1ad2d7a362ff53774c3b96724f68177
ParkJuhyun <wngus6429@gmail.com>
2023-10-28 18:45:30 +0900

    update

--- 사전2.py
+++ 사전2.py
@@ -1,8 +1,8 @@
-datadict = { "juhyun": "good", "whatscore":100}
+datadict = { "juhyun": "good", "whatscore": 100}
 print(datadict)
 for index in datadict:
     print(index)
-    print(datadict[index]);
+    print(datadict[index])
 
 
     
